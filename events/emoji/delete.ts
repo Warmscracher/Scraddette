da6d93657a92391a9c02bc62b1b875e11fdf9c3c
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-16 05:37:02 -0500

    Update delete.ts

--- events/emoji/delete.ts
+++ events/emoji/delete.ts
@@ -4,6 +4,6 @@ import type Event from "../../common/types/event";
 
 const event: Event<"emojiDelete"> = async function event(emoji) {
 	if (emoji.guild.id !== CONSTANTS.guild.id) return;
-	await log(`🙁 Emoji ${emoji.toString()} deleted!`, "server");
+	await log(`<:deleteemoji:1041829127184990208> Emoji ${emoji.toString()} deleted!`, "server");
 };
 export default event;
