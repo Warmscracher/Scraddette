490cb06a03fb90ab4bc72e83fc40dc6cdb461923
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-16 05:46:15 -0500

    Update create.ts

--- events/guild/scheduledEvent/create.ts
+++ events/guild/scheduledEvent/create.ts
@@ -12,7 +12,7 @@ const event: Event<"guildScheduledEventCreate"> = async function event(event) {
 		end = event.scheduledEndAt;
 
 	await log(
-		`🗓 Event ${event.name} scheduled${
+		`<:createevent:1042389397879926824> Event ${event.name} scheduled${
 			start || end
 				? ` for ${time(start || end || new Date())}${end && start ? "-" + time(end) : ""}`
 				: ""
