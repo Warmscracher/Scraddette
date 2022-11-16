4eaaa2d2927c00f64d278e729001fda97ac8617f
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-16 06:01:37 -0500

    Update delete.ts

--- events/role/delete.ts
+++ events/role/delete.ts
@@ -4,6 +4,6 @@ import type Event from "../../common/types/event";
 
 const event: Event<"roleDelete"> = async function event(role) {
 	if (role.guild.id !== CONSTANTS.guild.id) return;
-	await log(`🗄 Role @${role.name} deleted! (ID: ${role.id})`, "server");
+	await log(`<:deleterole:1041830305281097748> Role @${role.name} deleted! (ID: ${role.id})`, "server");
 };
 export default event;
