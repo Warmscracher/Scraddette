46d3d47a173c6634dfd51f59b3281a39b35dedb7
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-27 11:25:31 -0500

    Update update.ts

--- events/emoji/update.ts
+++ events/emoji/update.ts
@@ -4,6 +4,6 @@ import type Event from "../../common/types/event";
 
 const event: Event<"emojiUpdate"> = async function event(oldEmoji, newEmoji) {
 	if (newEmoji.name === oldEmoji.name || newEmoji.guild.id !== CONSTANTS.guild.id) return;
-	await log(`😶 Emoji ${oldEmoji.toString()} renamed to :${newEmoji.name}:!`, "server");
+	await log(`<:updateemoji:1041829065298022510> Emoji ${oldEmoji.toString()} renamed to :${newEmoji.name}:!`, "server");
 };
 export default event;
