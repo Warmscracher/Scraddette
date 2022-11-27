515a26bd3e0053e801e8f660fe10ab6c00b74fe2
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-27 11:29:59 -0500

    <:join:1041828456578682961>

--- events/guild/member/add.ts
+++ events/guild/member/add.ts
@@ -8,25 +8,25 @@ import { rolesDatabase } from "./remove.js";
 
 const event: Event<"guildMemberAdd"> = async function event(member) {
 	if (member.guild.id !== CONSTANTS.guild.id) return;
-	await log(`👋 Member ${member.toString()} joined!`, "members");
+	await log(`<:join:1041828456578682961> Member ${member.toString()} joined!`, "members");
 
 	const greetings = [
-		`Everybody please welcome ${member.toString()} to ${
+		`<:join:1041828456578682961> Everybody please welcome ${member.toString()} to ${
 			CONSTANTS.guild.name
 		}; they’re our ${nth(CONSTANTS.guild.memberCount)} member!`,
-		`A big shoutout to ${member.toString()}, we’re glad you’ve joined us as our ${nth(
+		`<:join:1041828456578682961> A big shoutout to ${member.toString()}, we’re glad you’ve joined us as our ${nth(
 			CONSTANTS.guild.memberCount,
 		)} member!`,
-		`Here we go again… ${member.toString()} is here, our ${nth(
+		`<:join:1041828456578682961> Here we go again… ${member.toString()} is here, our ${nth(
 			CONSTANTS.guild.memberCount,
 		)} member!`,
-		`||Do I always have to let you know when there is a new member?|| ${member.toString()} is here (our ${nth(
+		`<:join:1041828456578682961> ||Do I always have to let you know when there is a new member?|| ${member.toString()} is here (our ${nth(
 			CONSTANTS.guild.memberCount,
 		)})!`,
-		`Is it a bird? Is it a plane? No, it’s ${member.toString()}, our ${nth(
+		`<:join:1041828456578682961> Is it a bird? Is it a plane? No, it’s ${member.toString()}, our ${nth(
 			CONSTANTS.guild.memberCount,
 		)} member!`,
-		`Welcome:tm: ${member.toString()}! You’re our ${nth(CONSTANTS.guild.memberCount)} member!`,
+		`<:join:1041828456578682961> Welcome:tm: ${member.toString()}! You’re our ${nth(CONSTANTS.guild.memberCount)} member!`,
 	];
 
 	await CONSTANTS.channels.welcome?.send(
