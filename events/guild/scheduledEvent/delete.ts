28b8443978d2a63407de892de7c9d897efb9b06c
Yande <113806118+YandeMC@users.noreply.github.com>
2022-11-27 11:34:02 -0500

    Update delete.ts

--- events/guild/scheduledEvent/delete.ts
+++ events/guild/scheduledEvent/delete.ts
@@ -7,6 +7,6 @@ const event: Event<"guildScheduledEventDelete"> = async function event(event) {
 	const guild = event.guild || (await client.guilds.fetch(event.guildId));
 	if (guild.id !== CONSTANTS.guild.id) return;
 
-	await log(`📅 Event ${event.name} removed!`, "voice");
+	await log(`<:deleteevent:1041829873842405426> Event ${event.name} removed!`, "voice");
 };
 export default event;
